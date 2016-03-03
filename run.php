2704970b68a756036fc64cf4183bd2091ec39836
Samuel Marchal <contact@zes.sx>
2016-03-03 14:32:13 +0100

    Add optional limits

--- run.php
+++ run.php
@@ -6,16 +6,19 @@ require_once 'vendor/autoload.php';
 use PHPHtmlParser\Dom;
 $dom = new Dom;
 
-for ($i = 1; $i <= 1500; $i++) {
+$from = isset($_GET['from']) ? intval($_GET['from']) : 1;
+$to   = isset($_GET['to'])   ? intval($_GET['to'])   : 1500;
+
+for ($i = $from; $i <= $to; $i++) {
 
 	$dom->load('http://www.senscritique.com/badge/'. $i);
 	$title = $dom->find('title')[0];
-	
+
 	if (preg_match('/^Badge :/', $title->text)) {
 		print '<a href="http://www.senscritique.com/badge/'. $i .'" class="badge">';
 			print '<img src="'. $dom->find('img.acvi-achievement')[0]->src .'">';
 			print ucfirst(preg_replace('/^Badge : (.+?) - SensCritique$/', '$1', $title->text));
 		print '</a>'. PHP_EOL;
 	}
-	
+
 }
