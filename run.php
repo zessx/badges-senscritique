d722b93e84947f783dc2ff932c8e393429ae3b51
zessx <contact@zes.sx>
2016-12-03 16:08:29 +0100

    Add badges 1360 ~ 1456
    Conflicts:
    	index.html
    	run.php

--- run.php
+++ run.php
@@ -1,6 +1,5 @@
 <?php
 
-// Required: PHPHtmlParse (https://packagist.org/packages/paquettg/php-html-parser)
 require_once 'vendor/autoload.php';
 
 use PHPHtmlParser\Dom;
@@ -9,16 +8,27 @@ $dom = new Dom;
 $from = isset($_GET['from']) ? intval($_GET['from']) : 1;
 $to   = isset($_GET['to'])   ? intval($_GET['to'])   : 1500;
 
+$blocked = array();
 for ($i = $from; $i <= $to; $i++) {
 
-	$dom->load('http://www.senscritique.com/badge/'. $i);
+	$url = sprintf('http://www.senscritique.com/badge/%u', $i);
+	$dom->load($url);
 	$title = $dom->find('title')[0];
 
+	if ($title->text == 'You have been blocked') {
+		$blocked[] = $url;
+	}
 	if (preg_match('/^Badge :/', $title->text)) {
-		print '<a href="http://www.senscritique.com/badge/'. $i .'" class="badge">';
-			print '<img src="'. $dom->find('img.acvi-achievement')[0]->src .'">';
-			print ucfirst(preg_replace('/^Badge : (.+?) - SensCritique$/', '$1', $title->text));
-		print '</a>'. PHP_EOL;
+		printf(
+			'<a href="%s" class="badge"><img src="%s">%s</a>' . PHP_EOL,
+			$url,
+			$dom->find('img.acvi-achievement')[0]->src,
+			ucfirst(preg_replace('/^Badge : (.+?) - SensCritique$/', '$1', $title->text))
+		);
 	}
 
+}
+
+if (count($blocked) > 0) {
+	printf('<br>Blocked:<br>%s', implode('<br>', $blocked));
 }
